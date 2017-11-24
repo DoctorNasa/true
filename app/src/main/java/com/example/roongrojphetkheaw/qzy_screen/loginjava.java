b660508b57b4ba648594ba43c6481bb83f61fbe7
Roongroj777 <roongroj777@gmail.com>
2017-11-24 21:33:03 +0700

    init

---
+++ app/src/main/java/com/example/roongrojphetkheaw/qzy_screen/loginjava.java
@@ -0,0 +1,50 @@
+package com.example.roongrojphetkheaw.qzy_screen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.view.LayoutInflater;
+
+import org.w3c.dom.Text;
+
+import static com.pchmn.materialchips.R2.id.container;
+
+
+/**
+ * Created by roongrojphetkheaw on 11/13/2017 AD.
+ */
+
+public class loginjava extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.login, container,false);
+
+        initListeners();
+
+        TextView txtpass = (TextView) rootview.findViewById(R.id.txtpass);
+        txtpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().setTransition(new registerjava()).commit();
+            }
+        });
+
+
+        return rootview;
+
+    }
+
+    private void initListeners() {
+
+    }
+
+
+}
+
